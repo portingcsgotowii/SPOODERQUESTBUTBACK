5667843219f6f33554d10fed8701a40ac683abcc
portingcsgotowii <116188761+portingcsgotowii@users.noreply.github.com>
2022-12-11 13:44:23 +0100

    Create usethis.lua

---
+++ usethis.lua
@@ -0,0 +1,27 @@
+getgenv().Settings = {
+        
+        Configuration = {
+            Hardcore = false,
+            AutoSelectHighest = true,
+            WaveDefense = false
+        },
+        
+        CustomDungeon = {
+            Name = "Aquatic Temple",
+            Difficulty = "Nightmare"
+        },
+    },
+
+    EggIsland == {
+        Enabled = false,
+        Difficulty = "Nightmare", -- "Easy" / "Nightmare"
+        Class = "Mage"
+    },
+
+    Autofarm == {
+        DisableDodging = false,
+        Walkspeed = 30, 
+        DistanceFromMobs = 50,
+        AttackRange = 55,
+    }
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/portingcsgotowii/SPOODERQUESTBUTBACK/main/spooderbackmain.lua')()
